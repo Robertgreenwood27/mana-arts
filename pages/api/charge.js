220db4cc68d09f0ed33e652ada65da1a31a37631
Robert Greenwood <RobertGreenwood27@gmail.com>
2023-05-04 13:40:57 -0600

    fixed async

--- pages/api/charge.js
+++ pages/api/charge.js
@@ -1,34 +1,43 @@
-const { amount, paymentMethodId, currency, name, address: { line1, city, state, postal_code, country }, phoneNumber, email } = req.body;
-
-
-try {
-  const customer = await stripe.customers.create({
-    name,
-    address: {
-      line1,
-      city,
-      state,
-      postal_code,
-      country,
-    },
-    phone: phoneNumber,
-    email,
-  });
+async function handlePayment(req, res) {
+  const { amount, paymentMethodId, currency, name, address: { line1, city, state, postal_code, country }, phoneNumber, email } = req.body;
   
-
-  const payment = await stripe.paymentIntents.create({
-    amount,
-    currency,
-    payment_method: paymentMethodId,
-    customer: customer.id,
-    confirm: true,
-  });
-
-  if (payment.status === "succeeded") {
-    res.status(200).json({ success: true });
-  } else {
-    res.status(400).json({ success: false, message: "Payment failed" });
+  try {
+    const customer = await stripe.customers.create({
+      name,
+      address: {
+        line1,
+        city,
+        state,
+        postal_code,
+        country,
+      },
+      phone: phoneNumber,
+      email,
+    });
+    
+  
+    const payment = await stripe.paymentIntents.create({
+      amount,
+      currency,
+      payment_method: paymentMethodId,
+      customer: customer.id,
+      confirm: true,
+    });
+  
+    if (payment.status === "succeeded") {
+      res.status(200).json({ success: true });
+    } else {
+      res.status(400).json({ success: false, message: "Payment failed" });
+    }
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
   }
-} catch (error) {
-  res.status(500).json({ success: false, message: error.message });
 }
+
+app.post('/payment', async (req, res) => {
+  try {
+    await handlePayment(req, res);
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+});
