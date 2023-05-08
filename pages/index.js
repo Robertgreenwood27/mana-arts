945f8d78c0222e70044ed2a194ead5d2b97def73
Robert Greenwood <RobertGreenwood27@gmail.com>
2023-05-08 13:08:37 -0600

    updated front page

--- pages/index.js
+++ pages/index.js
@@ -1,49 +1,105 @@
-import React from "react";
-import Link from "next/link";
 import Image from "next/image";
-import ProductCard from "../components/ProductCard";
-import client from "../sanity.config";
+import Link from "next/link";
+import { useState } from "react";
 
-export default function IndexPage({ products }) {
+export default function Home() {
+  const [logoSpin, setLogoSpin] = useState(false);
+
+  const handleLogoClick = () => {
+    setLogoSpin(true);
+    setTimeout(() => setLogoSpin(false), 1000); // stop spinning after 1 second
+  };
+
   return (
-    <div className="bg-zinc-900 flex flex-col justify-center items-center min-h-screen">
-      <div className="text-white text-center justify-center items-center">
-        <h1 className="text-4xl font-bold mb-4">Mana Arts</h1>
-        <Image src="/logo.png" alt="Logo" width={200} height={200} />
-      </div>
-      <div className="container mx-auto py-8">
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 sm:gap-6 lg:gap-8 justify-items-center">
-          {products.map((product) => (
-            <Link key={product._id} href={`/products/${product.slug.current}`} legacyBehavior>
-              <a>
-                <ProductCard product={product} />
-              </a>
-            </Link>
-          ))}
+    <div className="flex flex-col">
+      <section className="intro-section section flex flex-col justify-center items-center">
+        <div className="text-white text-center items-center">
+          <h1 className="text-4xl font-bold mb-4">Mana Arts</h1>
+          <div className={logoSpin ? "spin" : ""} onClick={handleLogoClick}>
+            <Image src="/logo.png" alt="Logo" width={200} height={200} />
+          </div>
         </div>
-      </div>
+      </section>
+      <section className="request-section section-alt min-h-screen flex items-center justify-center">
+        <Link href="/request" legacyBehavior>
+          <a className="bg-black text-white py-6 px-8 rounded-lg shadow-lg cursor-pointer border border-white text-center">
+            <p className="text-3xl font-bold mb-2">Request something</p>
+          </a>
+        </Link>
+      </section>
+      <section className="signin-section section min-h-screen flex items-center justify-center">
+        <Link href="/signin" legacyBehavior>
+          <a className="bg-black text-white py-6 px-8 rounded-lg shadow-lg cursor-pointer mt-8 border border-white text-center">
+            <p className="text-3xl font-bold mb-2">See your stuff</p>
+          </a>
+        </Link>
+      </section>
+      <section className="lookaround-section section-alt min-h-screen flex items-center justify-center">
+        <Link href="/signin" legacyBehavior>
+          <a className="bg-black text-white py-6 px-8 rounded-lg shadow-lg cursor-pointer mt-8 border border-white text-center">
+            <p className="text-3xl font-bold mb-2">Look around</p>
+          </a>
+        </Link>
+      </section>
+
+      <style jsx>{`
+        .spin {
+          animation: spin 1s;
+        }
+        @keyframes spin {
+          from {
+            transform: rotate(0deg);
+          }
+          to {
+            transform: rotate(360deg);
+          }
+        }
+        .section {
+          clip-path: polygon(0% 6%, 100% 0%, 100% 94%, 0% 100%);
+          margin-bottom: -30%;
+          padding-top: 6%;
+          overflow: hidden;
+        }
+        @media (min-width: 768px) {
+          .section {
+            clip-path: polygon(0% 4%, 100% 0%, 100% 96%, 0% 100%);
+            margin-bottom: -15%;
+            padding-top: 4%;
+          }
+          .section-alt {
+            clip-path: polygon(0% 0%, 100% 4%, 100% 100%, 0% 96%);
+            margin-bottom: -15%;
+            padding-top: 4%;
+          }
+          .intro-section {
+            margin-top: -1.5%;
+            padding-top: 4%;
+            padding-bottom: 15%;
+          }
+        }
+        .request-section {
+          background-image: url('/redgrey.png');
+          background-size: cover;
+          background-position: center;
+          padding-top: 1%;
+        }
+        .signin-section {
+          background-image: url('/purple.png');
+          background-size: cover;
+          background-position: center;
+          padding-top: 1%;
+        }
+        .lookaround-section {
+          background-image: url('/green.png');
+          background-size: cover;
+          background-position: center;
+          clip-path: polygon(0% 0%, 100% 6%, 100% 100%, 0% 100%);
+          padding-top: 1%;
+        }
+        
+        
+        
+      `}</style>
     </div>
   );
-}
-
-export async function getStaticProps() {
-  const products = await client.fetch(
-    `*[_type == "product"]{
-      _id,
-      name,
-      price,
-      description,
-      "image": image.asset->url,
-      slug {
-        current
-      }
-    }`
-  );
-
-  return {
-    props: {
-      products,
-    },
-    revalidate: 600,
-  };
 }
