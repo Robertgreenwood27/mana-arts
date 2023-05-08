2af8d2f11ef3c7a2bec90104b2778a4f09ba117f
Robert Greenwood <RobertGreenwood27@gmail.com>
2023-05-08 16:05:08 -0600

    tried fixing emailer

--- pages/utils/email.js
+++ pages/utils/email.js
@@ -1,16 +1,17 @@
+// pages/utils/email.js
 const nodemailer = require("nodemailer");
 
 async function sendEmail({ to, subject, text }) {
   const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
-      user: process.env.EMAIL_ADDRESS,
-      pass: process.env.EMAIL_PASSWORD,
+      user: process.env.NEXT_PUBLIC_EMAIL_ADDRESS,
+      pass: process.env.NEXT_PUBLIC_EMAIL_PASSWORD,
     },
   });
 
   const mailOptions = {
-    from: process.env.EMAIL_ADDRESS,
+    from: process.env.NEXT_PUBLIC_EMAIL_ADDRESS,
     to,
     subject,
     text,
