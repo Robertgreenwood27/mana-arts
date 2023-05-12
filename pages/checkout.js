22c626f021e21cc59c7301a76ad66afee0c52eaf
Robert Greenwood <RobertGreenwood27@gmail.com>
2023-05-12 11:20:39 -0600

    changed live stripe key

--- pages/checkout.js
+++ pages/checkout.js
@@ -5,7 +5,8 @@ import { loadStripe } from "@stripe/stripe-js";
 import CheckoutForm from "../components/CheckoutForm";
 import CartContext from "../components/CartContext";
 
-const stripePromise = loadStripe("pk_test_51N35ZvCpcAu4mxfZrAz4Ns4YypKsWB0bZ6KxZrgnTKIU7zMBTHPXx630kc8AxTxzkQAjx1tae9I1zhmQdrDypZzM00GzuWICnm");
+const stripePromise = loadStripe("pk_live_51N35ZvCpcAu4mxfZRYHy1lhSexODaPoEi1BcoJEf6PGVj0sQztPXthHcR9AskLzzkRPIIvP9gcppLU8ypiGoN8tC00FEVQ9wSf");
+
 
 export default function Checkout() {
   const { totalCost, cartItems } = useContext(CartContext);
