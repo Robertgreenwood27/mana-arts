34f5356d1ef72d11d3acea623ca1bf83ee9523f8
Robert Greenwood <RobertGreenwood27@gmail.com>
2023-05-12 20:03:18 -0600

    removed google maps

--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,6 @@ class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link href="https://fonts.googleapis.com/css2?family=Amarante&display=swap" rel="stylesheet" />
-          <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCadyehzkGXRqxLlwtxkDQVoqfWNRJ2I8I"></script>
         </Head>
         <body>
           <Main />
