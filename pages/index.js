14ee155801c862dfc7ffa632764a41281e40ab33
Robert Greenwood <RobertGreenwood27@gmail.com>
2023-05-28 06:40:37 -0600

    small changes

--- pages/index.js
+++ pages/index.js
@@ -39,14 +39,14 @@ export default function Home({ categories }) {
       <section className="request-section min-h-screen flex items-center justify-center" style={{backgroundImage: "url('/green.png')", backgroundSize: "cover", backgroundPosition: "center"}}>
         <Link href="/request" legacyBehavior>
           <a className="bg-black text-white py-6 px-8 rounded-lg shadow-lg cursor-pointer border border-white text-center">
-            <p className="text-3xl font-bold mb-2">Request something</p>
+            <p className="text-3xl font-bold mb-2">MAKE A REQUEST</p>
           </a>
         </Link>
       </section>
       <section className="signin-section min-h-screen flex items-center justify-center" style={{backgroundImage: "url('/purple.png')", backgroundSize: "cover", backgroundPosition: "center"}}>
         <Link href="/signIn" legacyBehavior>
           <a className="bg-black text-white py-6 px-8 rounded-lg shadow-lg cursor-pointer mt-8 border border-white text-center">
-            <p className="text-3xl font-bold mb-2">See your stuff</p>
+            <p className="text-3xl font-bold mb-2">SEE YOUR STUFF</p>
           </a>
         </Link>
       </section>
