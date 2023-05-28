d3483f3bfac3e99a2daec04dfd9766529ff903af
Robert Greenwood <RobertGreenwood27@gmail.com>
2023-05-27 18:34:14 -0600

    changed up index

---
+++ components/Carousel.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+
+
+const responsive = {
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 3,
+    slidesToSlide: 1
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2,
+    slidesToSlide: 1
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+    slidesToSlide: 1
+  }
+}
+
+export default function CarouselComponent({ categories }) {
+  return (
+    <Carousel
+      swipeable={false}
+      draggable={false}
+      showDots={false}
+      responsive={responsive}
+      ssr={true} 
+      infinite={true}
+      autoPlay={true}
+      autoPlaySpeed={1000}
+      keyBoardControl={true}
+      customTransition="all 1s ease-in"
+      transitionDuration={1000}
+      containerClass="carousel-container"
+      dotListClass="custom-dot-list-style"
+      itemClass="carousel-item-padding-40-px"
+    >
+      {categories.map((category) => (
+        <div key={category._id}>
+          <img 
+            src={category.imageUrl} 
+            alt={category.name}
+            className="object-contain w-full h-auto" 
+          />
+          <p className="legend">{category.name}</p>
+        </div>
+      ))}
+    </Carousel>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,42 @@
 import Link from "next/link";
 import SpinningLogo from "../components/SpinningLogo"
+import Carousel from "../components/Carousel"
+import client from "../sanity.config"; // import client
 
-
-export default function Home() {
+export default function Home({ categories }) {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col @container">
       <section className="intro-section section flex flex-col justify-center items-center">
           <SpinningLogo />
       </section>
-      <section className="request-section min-h-screen flex items-center justify-center" style={{backgroundImage: "url('/redgrey.png')", backgroundSize: "cover", backgroundPosition: "center"}}>
+      <section className="services-section @lg:py-6 m-6">
+        <div className="grid grid-cols-1 @lg:grid-cols-2 @xl:grid-cols-4 gap-4">
+    <div className="service-card">
+      <img src="customprint.png" alt="service 1" className="rounded"/>
+      <h3 className="text-center text-xl mt-4">Custom Printing</h3>
+      <p className="text-center">High-quality and efficient services are available for a variety of products.</p>
+    </div>
+    <div className="service-card">
+      <img src="customlogos.png" alt="service 2" className="rounded"/>
+      <h3 className="text-center text-xl mt-4">Logo Design</h3>
+      <p className="text-center">Unique and impactful logos to represent your brand.</p>
+    </div>
+    <div className="service-card">
+      <img src="imagemanipulation.png" alt="service 3" className="rounded"/>
+      <h3 className="text-center text-xl mt-4">Image Manipulation</h3>
+      <p className="text-center">Professional-grade image manipulation to enhance your visuals.</p>
+    </div>
+    <div className="service-card">
+      <img src="webdev.png" alt="service 4" className="rounded"/>
+      <h3 className="text-center text-xl mt-4">Web Development</h3>
+      <p className="text-center">Responsive and user-friendly websites.</p>
+    </div>
+  </div>
+</section>
+
+
+      <Carousel categories={categories} style={{backgroundImage: "url('/redgrey.png')", backgroundSize: "cover", backgroundPosition: "center"}}/>
+      <section className="request-section min-h-screen flex items-center justify-center" style={{backgroundImage: "url('/green.png')", backgroundSize: "cover", backgroundPosition: "center"}}>
         <Link href="/request" legacyBehavior>
           <a className="bg-black text-white py-6 px-8 rounded-lg shadow-lg cursor-pointer border border-white text-center">
             <p className="text-3xl font-bold mb-2">Request something</p>
@@ -22,13 +50,23 @@ export default function Home() {
           </a>
         </Link>
       </section>
-      <section className="lookaround-section min-h-screen flex items-center justify-center" style={{backgroundImage: "url('/green.png')", backgroundSize: "cover", backgroundPosition: "center"}}>
-        <Link href="/general" legacyBehavior>
-          <a className="bg-black text-white py-6 px-8 rounded-lg shadow-lg cursor-pointer mt-8 border border-white text-center">
-            <p className="text-3xl font-bold mb-2">Look around</p>
-          </a>
-        </Link>
-      </section>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const categories = await client.fetch(
+    `*[_type == "category"]{
+      _id,
+      name,
+      "slug": slug.current,
+      "imageUrl": image.asset->url
+    }`
+  );
+  return {
+    props: {
+      categories,
+    },
+    revalidate: 60,
+  };
 }
